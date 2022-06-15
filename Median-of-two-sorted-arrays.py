5ec2173fa0908387a6bbed11cfd11233ee685831
Zanete Dijeva <zanete.dijeva@gmail.com>
2022-06-15 23:31:57 +0100

    Practice

---
+++ Median-of-two-sorted-arrays.py
@@ -0,0 +1,83 @@
+'''
+Given two sorted arrays nums1 and nums2 of size m and n respectively,
+return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+'''
+#nums1 = (1,2), nums2 = (3,4)
+'''
+def findMedianSortedArrays(self, nums1, nums2):
+    nums3 = {}
+    for i in range(len(nums1)) and j in range(len(nums2)):
+        if num1 >= num2 :
+            nums3[num1] =
+            i += 1
+        elif num2 >= num1 :
+            nums3[num2] = i
+        else : continue
+    return nums3
+'''
+
+#findMedianSortedArrays(self, nums1, nums2)
+
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+
+        # Iterator for the main list
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+
+myList = [54,26,93,17,77,31,44,55,20]
+mergeSort(myList)
+print(myList)
